5d8ce1c396519fbbbf20f215a524517a182cc07e
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2021-03-03 13:47:40 +0100

    Fix conversation creator (was not in fillable)

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'creator_id'];
 
     public function creator() {
         return $this->belongsTo(User::class);
